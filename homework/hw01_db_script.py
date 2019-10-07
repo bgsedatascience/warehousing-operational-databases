39672209478c150ce5cf27e837ce614339c77f36
msalunina <msalunina@gmail.com>
2019-10-07 11:34:20 +0200

    Changed function create_table

--- homework/hw01_db_script.py
+++ homework/hw01_db_script.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import dataset
 
+
 def create_table(df, columns):
     table = df[columns].copy()
     table = table.drop_duplicates().reset_index(drop=True)
+    table = table.where((pd.notnull(table)), None)
     return table.to_dict('records')
 
-products_table = pd.read_csv('extracts/products.csv')
-products_table = products_table.where((pd.notnull(products_table)), None).to_dict('records')
+
+# data processing
+products = pd.read_csv('extracts/products.csv')
+products_table = create_table(products, products.columns)
 
 employees = pd.read_csv('extracts/employees.csv')
-employees = employees.where((pd.notnull(employees)), None)
 offices_columns = ['office_code', 'city', 'state', 'country', 'office_location']
 employees_columns = ['employee_number', 'last_name', 'first_name', 'reports_to', 'job_title', 'office_code']
 offices_table = create_table(employees, offices_columns)
 employees_table = create_table(employees, employees_columns)
 
 orders = pd.read_csv('extracts/orders.csv')
-orders = orders.where((pd.notnull(orders)), None)
 customers_columns = ['customer_number', 'customer_name', 'contact_last_name', 'contact_first_name',
                      'city', 'state', 'country', 'credit_limit', 'customer_location']
 orders_columns = ['order_number', 'customer_number', 'order_date', 'required_date',
@@ -29,6 +31,7 @@ orders_table = create_table(orders, orders_columns)
 items_table = create_table(orders, items_columns)
 
 
+# data loading
 data_base = dataset.connect("postgresql://postgres@localhost/data_base_hw01")
 
 products = data_base['products']
